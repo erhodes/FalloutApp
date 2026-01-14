59971235c7ba6168d2ea4c4da929dc013c02f637
Eric Rhodes <eric.rhodes@cplcloud.com>
2026-01-14 17:25:29 -0500

    added a UserDataSource

---
+++ composeApp/src/commonMain/kotlin/com/erhodes/falloutapp/data/UserDataSource.kt
@@ -0,0 +1,9 @@
+package com.erhodes.falloutapp.data
+
+// This class is going to have some server calls in it. They might make more sense elsewhere.
+class UserDataSource {
+
+    fun submitLoginRequest(name: String) {
+
+    }
+}
