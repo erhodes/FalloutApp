67f73b5c10d49c6d94fc4d5437ce1b39b79eb4c6
ericrhodes <e_rhodes@live.com>
2026-01-29 16:43:09 -0500

    Added more perks.

--- composeApp/src/commonMain/kotlin/com/erhodes/falloutapp/data/PerkDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/erhodes/falloutapp/data/PerkDataSource.kt
@@ -94,5 +94,81 @@ object PerkDataSource {
             StatRequirement(Stats.STRENGTH, 2),
             SkillRequirement(Skills.MELEE, 5)
         )
+        perkMap[12] = Perk(
+            name = "Sprinter",
+            description = "Increase your speed by 2.",
+            id = 12,
+            SkillRequirement(Skills.ATHLETICS, 5)
+        )
+        perkMap[13] = Perk(
+            name = "Gadgeteer",
+            description = "Once per day as an Activity, spend 1 Part to create a number of makeshift Gadgets with total crafting cost equal to your Engineering skill. You must have access to a scrapper’s lab or kit to use this Activity.",
+            id = 13,
+            SkillRequirement(Skills.ENGINEERING, 5)
+        )
+        perkMap[14] = Perk(
+            "Field Surgeon",
+            description = "When you perform the First Aid activity, you may spend up to 2/4/6 supplies, and you may split the healing among multiple patients.",
+            id = 14,
+            SkillRequirement(Skills.MEDICINE, 5)
+        )
+        perkMap[15] = Perk(
+            name = "Locksmith",
+            description = "When lockpicking, you may 1/3/5 guesses.",
+            id = 15,
+            SkillRequirement(Skills.SABOTAGE, 5)
+        )
+        perkMap[16] = Perk(
+            name = "Chemist",
+            description = "Once per day as an Activity, spend 1 Medical Supply to create a number of makeshift Chems with total crafting cost equal to your Science skill. You must have access to a chemist’s lab or kit to use this Activity.",
+            id = 16,
+            SkillRequirement(Skills.SCIENCE, 5)
+        )
+        perkMap[17] = Perk(
+            name = "Ghost",
+            description = "Add 1/1/2 to Hide checks.",
+            id = 17,
+            SkillRequirement(Skills.SNEAK, 5)
+        )
+        perkMap[18] = Perk(
+            name = "Insightful",
+            description = "After making a persuasion test against someone in which you scored at least 1 success, learn one of their motivations.",
+            id = 18,
+            StatRequirement(Stats.CHARISMA, 2),
+            SkillRequirement(Skills.SPEECH, 5)
+        )
+        perkMap[19] = Perk(
+            name = "With Me!",
+            description = "When allies use Follow the Leader on your turn, they may Run instead of Dash.",
+            id = 19,
+            StatRequirement(Stats.CHARISMA, 2),
+            SkillRequirement(Skills.SPEECH, 5)
+        )
+        perkMap[20] = Perk(
+            name = "Frequent Flyer",
+            description = "Lower Fatigue inflicted by Chems by 1.",
+            id = 20,
+            StatRequirement(Stats.ENDURANCE, 2),
+            SkillRequirement(Skills.SURVIVAL, 5)
+        )
+        perkMap[21] = Perk(
+            name = "Lucky Find",
+            description = "You may add or subtract up to 2 on any Loot roll",
+            id = 21,
+            StatRequirement(Stats.LUCK, 2)
+        )
+        perkMap[22] = Perk(
+            name = "Scavenger",
+            description = "Add 1/1/2 to Scavenge rolls.",
+            id = 22,
+            StatRequirement(Stats.PERCEPTION, 2),
+        )
+        perkMap[23] = Perk(
+            name = "Inspirational",
+            description = "Spend 1AP: Until the start of your next turn, you may use Aid without spending a Reaction.",
+            id = 23,
+            StatRequirement(Stats.CHARISMA, 2),
+            SkillRequirement(Skills.SPEECH, 5)
+        )
     }
 }
