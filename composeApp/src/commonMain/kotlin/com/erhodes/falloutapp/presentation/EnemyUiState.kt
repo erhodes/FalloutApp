c79427dba9b1234067886835f2b4c681a9bdc94b
Eric Rhodes <e_rhodes@live.com>
2026-04-22 20:07:55 -0400

    Fixed a UI update bug

--- composeApp/src/commonMain/kotlin/com/erhodes/falloutapp/presentation/EnemyUiState.kt
+++ composeApp/src/commonMain/kotlin/com/erhodes/falloutapp/presentation/EnemyUiState.kt
@@ -1,7 +1,6 @@
 package com.erhodes.falloutapp.presentation
 
 import com.erhodes.falloutapp.model.Character
-import com.erhodes.falloutapp.model.action.Action
 
 data class EnemyUiState(
     val index: Int,
